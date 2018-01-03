bca491c08ea2e43e360ae4826922dfb1dd35b7c2
maxscores <maxstackhouse@gmail.com>
2018-01-03 07:47:09 -0700

    Expand Test

--- spec/features/user_can_view_all_items_spec.rb
+++ spec/features/user_can_view_all_items_spec.rb
@@ -6,11 +6,44 @@ describe "user can view all items" do
 
     visit items_path
 
-    expect(page).to have_content(item1.title)
-    expect(page).to have_content(item1.price)
-    expect(page).to have_content(item2.title)
-    expect(page).to have_content(item2.price)
-    expect(page).to have_content(item3.title)
-    expect(page).to have_content(item3.price)
+    expect(page).to have_link(item1.title)
+    expect(page).to have_link(item1.image)
+    expect(page).to have_content(item1.price_in_dollars)
+    expect(page).to have_link(item2.image)
+    expect(page).to have_link(item2.title)
+    expect(page).to have_content(item2.price_in_dollars)
+    expect(page).to have_link(item3.title)
+    expect(page).to have_link(item3.price)
+    expect(page).to have_content(item3.price_in_dollars)
+  end
+
+  context "page has more than 15 items" do
+    it "shows pagination buttons" do
+      17.times do
+        create(:item)
+      end
+
+      visit items_path
+
+      within(".paginate-container") do
+        expect(page).to have_content("Previous")
+        expect(page).to have_content("Next")
+      end
+    end
+  end
+
+  context "page has less than 15 items" do
+    it "does not show pagination buttons" do
+      7.times do
+        create(:item)
+      end
+
+      visit items_path
+
+      within(".paginate-container") do
+        expect(page).to_not have_content("Previous")
+        expect(page).to_not have_content("Next")
+      end
+    end
   end
 end
