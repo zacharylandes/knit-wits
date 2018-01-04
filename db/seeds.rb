8d5f0ab3dcc234e1e6c4d537022fa8fcbe145c20
maxscores <maxstackhouse@gmail.com>
2018-01-04 16:11:45 -0700

    asset path

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,113 +7,113 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-mittens = Category.create(name: "Mittens", image: "products/thrummed_mitts1.jpg")
-hats = Category.create(name: "Hats", image: "products/green_cables_hat.JPG")
-sweaters = Category.create(name: "Sweaters", image: "products/baby_sweater2.JPG")
-cowls = Category.create(name: "Cowls", image: "products/blue_shawl1.JPG")
+mittens = Category.create(name: "Mittens", image: asset_path("products/thrummed_mitts1.jpg"))
+hats = Category.create(name: "Hats", image: asset_path("products/green_cables_hat.JPG"))
+sweaters = Category.create(name: "Sweaters", image: asset_path("products/baby_sweater2.JPG"))
+cowls = Category.create(name: "Cowls", image: asset_path("products/blue_shawl1.JPG"))
 
 Item.create!(title: 'Thrummed Mitt',
   description: "Super warm mitts with extra wooly bits inside",
   price: 1000,
-  image: "products/thrummed_mitts1.jpg",
+  image: asset_path("products/thrummed_mitts1.jpg"),
   category: mittens,
   status: 1)
 
 Item.create!(title: "Anna's Favorite",
   description: "Purple wooly hat perfect for a newborn",
   price: 1000,
-  image: "products/anna_hat.JPG",
+  image: asset_path("products/anna_hat.JPG"),
   category: hats,
   status: 1)
 
 Item.create!(title: "Antlers",
   description: "Are those real antlers? Of course not, they're made of wool",
   price: 1500,
-  image: "products/antler_hat.JPG",
+  image: asset_path("products/antler_hat.JPG"),
   category: hats,
   status: 1)
 
 Item.create!(title: "Baby Button-Up",
   description: "Blue baby sweater",
   price: 7500,
-  image: "products/baby_sweater2.JPG",
+  image: asset_path("products/baby_sweater2.JPG"),
   category: sweaters,
   status: 1)
 
 Item.create!(title: "Badger",
   description: "Honey Badger Don't Give a F@CK",
   price: 2000,
-  image: "products/badger_mitts4.JPG",
+  image: asset_path("products/badger_mitts4.JPG"),
   category: mittens,
   status: 1)
 
 Item.create!(title: "Bam Pow",
   description: "Inspired from retro Batman!",
   price: 2000,
-  image: "products/bam_pow.JPG",
+  image: asset_path("products/bam_pow.JPG"),
   category: mittens,
   status: 1)
 
 Item.create!(title: "Blue Cardigan",
   description: "Wool cardigan with pattern on arms",
   price: 10000,
-  image: "products/blue_cardi4.JPG",
+  image: asset_path("products/blue_cardi4.JPG"),
   category: sweaters,
   status: 1)
 
 Item.create!(title: "Goldenrod",
   description: "What a cool pattern!!",
   price: 10000,
-  image: "products/goldenrod_sweater.JPG",
+  image: asset_path("products/goldenrod_sweater.JPG"),
   category: sweaters,
   status: 1)
 
 Item.create!(title: "Beautiful Blue",
   description: "Your friends will say Wowzers!!",
   price: 4500,
-  image: "products/blue_shawl1.JPG",
+  image: asset_path("products/blue_shawl1.JPG"),
   category: cowls,
   status: 1)
 
 Item.create!(title: "Foxy",
   description: "NOT A REAL FOX",
   price: 4000,
-  image: "products/fox_scarf.jpg",
+  image: asset_path("products/fox_scarf.jpg"),
   category: cowls,
   status: 1)
 
 Item.create!(title: "Joan of Arc",
   description: "Lead your armies to victory",
   price: 4500,
-  image: "products/joan_of_arc_scarf.jpg",
+  image: asset_path("products/joan_of_arc_scarf.jpg"),
   category: cowls,
   status: 1)
 
 Item.create!(title: "Owl You Warm?",
   description: "I'm not just warm, I'm HOOT!",
   price: 2500,
-  image: "products/owl_mitts.JPG",
+  image: asset_path("products/owl_mitts.JPG"),
   category: mittens,
   status: 1)
 
 Item.create!(title: "Space Invader Sweater",
   description: "Bring back your favorite game with this gem!",
   price: 10000,
-  image: "products/space_invaders2.JPG",
+  image: asset_path("products/space_invaders2.JPG"),
   category: sweaters,
   status: 1)
 
 Item.create!(title: "Hand Puppet",
   description: "Who won't want to talk to your hand with these mittens",
   price: 1500,
-  image: "products/hand_puppet1.JPG",
+  image: asset_path("products/hand_puppet1.JPG"),
   category: mittens,
   status: 1)
 
 Item.create!(title: "Seaside Cowl",
   description: "Seaside inspired cowl, super warm",
   price: 2500,
-  image: "products/seaside_cowl2.JPG",
+  image: asset_path("products/seaside_cowl2.JPG"),
   category: cowls,
   status: 1)
 
@@ -121,6 +121,6 @@ Item.create!(title: "Seaside Cowl",
 Item.create!(title: "Green Cables Hat",
   description: "Warm green hat with thick styles",
   price: 2500,
-  image: "products/green_cables_hat.JPG",
+  image: asset_path("products/green_cables_hat.JPG"),
   category: hats,
   status: 1)
