b83a10ebe73dc9e9e03250c6f1ee79fb23f77a2d
ZacharyLandes <zach.g.landes@gmail.com>
2018-01-03 18:50:13 -0700

    nav and footer testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items, only: [:index, :show]
+
   resources :categories, only: [:show]
+
+  get '/', to: 'items#index'
+
 end
