a05ebf0da87f9bc69076c5523e02906a1453e4b5
maxscores <maxstackhouse@gmail.com>
2018-01-04 15:49:37 -0700

    Remove Foundation JS

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20180104223431) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.integer "role"
+    t.integer "role", default: 0
   end
 
 end
