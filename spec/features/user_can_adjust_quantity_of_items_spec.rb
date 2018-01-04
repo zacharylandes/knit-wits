a8620ffb6d1e04637e945a152429512957d96bd2
Kathrine <katy.jane8@gmail.com>
2018-01-04 11:38:51 -0700

    Update user_can_adjust_quantity_of_items_spec.rb

--- spec/features/user_can_adjust_quantity_of_items_spec.rb
+++ spec/features/user_can_adjust_quantity_of_items_spec.rb
@@ -18,25 +18,11 @@ describe "Visitor views their cart" do
 
       click_button "Add to Cart"
 
-
       click_link "Cart"
-
 
       fill_in "quantity", with: 4
 
       click_on "Save changes"
-
       expect(page).to have_content("You now have 4 cool mittens in your cart!")
     end
-    # it "visitor can add an amount to their cart" do
-    #   visit items_path
-    #
-    #   click_button "Add to Cart"
-    #
-    #   click_link "Cart"
-    #
-    #   click_button 'Add one'
-    #
-    #   expect(page).to have_content("You now have 0 cool mittens in your cart!")
-    # end
 end
