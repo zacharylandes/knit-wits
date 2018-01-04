5823c29ef33d089bcc36cf6e8b8189782893a892
maxscores <maxstackhouse@gmail.com>
2018-01-03 17:41:10 -0700

    Added 12 items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,79 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-category = Category.create(name: "Mittens", id: 2)
-item = Item.create!(title: 'Thrummed Mitts',
+mittens = Category.create(name: "Mittens")
+hats = Category.create(name: "Hats")
+sweaters = Category.create(name: "Sweaters")
+cowls = Category.create(name: "Cowls")
+
+Item.create!(title: 'Thrummed Mitt',
   description: "Super warm mitts with extra wooly bits inside",
-  price: 1000, image: "products/thrummed_mitts1.jpg",
-  category_id: 2)
+  price: 1000,
+  image: "products/thrummed_mitts1.jpg",
+  category: mittens)
+
+Item.create!(title: "Anna's Favorite",
+  description: "Purple wooly hat perfect for a newborn",
+  price: 1000,
+  image: "products/anna_hat.jpg",
+  category: hats)
+
+Item.create!(title: "Antlers",
+  description: "Are those real antlers? Of course not, they're made of wool",
+  price: 1500,
+  image: "products/antler_hat.jpg",
+  category: hats)
+
+Item.create!(title: "Baby Button-Up",
+  description: "Blue baby sweater",
+  price: 7500,
+  image: "products/baby_sweater2.jpg",
+  category: sweaters)
+
+Item.create!(title: "Badger",
+  description: "Honey Badger Don't Give a F@CK",
+  price: 2000,
+  image: "products/badger_mitts4.jpg",
+  category: mittens)
+
+Item.create!(title: "Bam Pow",
+  description: "Inspired from retro Batman!",
+  price: 2000,
+  image: "products/bam_pow.jpg",
+  category: mittens)
+
+Item.create!(title: "Blue Cardigan",
+  description: "Wool cardigan with pattern on arms",
+  price: 10000,
+  image: "products/blue_cardi4.jpg",
+  category: sweaters)
+
+Item.create!(title: "Goldenrod",
+  description: "What a cool pattern!!",
+  price: 10000,
+  image: "products/goldenrod_sweater.jpg",
+  category: sweaters)
+
+Item.create!(title: "Beautiful Blue",
+  description: "Your friends will say Wowzers!!",
+  price: 4500,
+  image: "products/blue_shawl1.jpg",
+  category: cowls)
+
+Item.create!(title: "Foxy",
+  description: "NOT A REAL FOX",
+  price: 4000,
+  image: "products/fox_scarf.jpg",
+  category: cowls)
+
+Item.create!(title: "Joan of Arc",
+  description: "Lead your armies to victory",
+  price: 4500,
+  image: "products/joan_of_arc_scarf.jpg",
+  category: cowls)
+
+Item.create!(title: "Owl You Warm?",
+  description: "I'm not just warm, I'm HOOT!",
+  price: 2500,
+  image: "products/owl_mitts.jpg",
+  category: mittens)
