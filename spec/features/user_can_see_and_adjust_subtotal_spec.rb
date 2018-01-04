1add1e047a85cd55cf3c6907788cf1a0bb4ac95d
katyjane8 <katy.jane8@gmail.com>
2018-01-04 13:19:44 -0700

    tests pass with quantity method

--- spec/features/user_can_see_and_adjust_subtotal_spec.rb
+++ spec/features/user_can_see_and_adjust_subtotal_spec.rb
@@ -23,9 +23,12 @@ describe 'user can view cart total' do
       total = @item.price * 2
 
       click_link "Cart"
+      fill_in "quantity", with: 4
+      click_on "Save changes"
 
+      expect(page).to have_content(4)
       expect(current_path).to eq(carts_path)
-      expect(page).to have_content("Knit Total Cost: $100.00")
+      expect(page).to have_content("Knit Total Cost: $200.00")
     end
   end
 end
