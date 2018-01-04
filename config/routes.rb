8e416f9562c23c822f8841b7c074c50c0cb2e0c4
ZacharyLandes <zach.g.landes@gmail.com>
2018-01-03 18:42:08 -0700

    nav and footer tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   delete '/cart', to: "carts#destroy"
   resources :items, only: [:index, :show]
-  resources :categories, only: [:show]
+  resources :categories, only: [ :index, :show]
   resources :carts, only: [:index, :create]
-
+  get '/', to: "items#index"
 end
