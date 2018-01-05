c04d926db1f9ff829c1e9921326f6e9459f859b7
ZacharyLandes <zach.g.landes@gmail.com>
2018-01-04 17:52:47 -0700

    merge with create user

---
+++ spec/features/user_can_sign_up_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+describe User do
+  describe 'user authentication' do
+  it 'signs up ' do
+    visit '/sign-up'
+
+
+  fill_in "user[username]", with: "funbucket13"
+  fill_in "user[password]", with: "test"
+
+  click_on "Create User"
+
+  expect(page).to have_content("Welcome, funbucket13!")
+    end
+  end
+
+  describe 'already logged in' do
+    xit 'is already logged in' do
+     user = create(:user)
+    visit '/'
+
+    click_on "Login"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in "username", with: user.username
+    fill_in "password", with: user.password
+
+    click_on "Log In"
+
+    expect(current_path).to eq(user_path(user))
+
+    expect(page).to have_content("Welcome, username!")
+    expect(page).to have_content("Logout")
+    end
+
+    it "can be created as an admin" do
+      user = create(:user, role:1)
+
+      expect(user.role).to eq("admin")
+      expect(user.admin?).to be_truthy
+    end
+
+    it "can be created as a default user" do
+      user = create(:user)
+
+      expect(user.role).to eq("default")
+      expect(user.default?).to be_truthy
+    end
+  end
+end
