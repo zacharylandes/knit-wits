c1f6709dcdc8bbe6bfdcc4e3c87472e248923a25
ZacharyLandes <zach.g.landes@gmail.com>
2018-01-06 10:28:06 -0700

    merge

---
+++ spec/features/user_can_add_items_to_cart_spec.rb
@@ -0,0 +1,27 @@
+describe "Visitor sees item they like" do
+  describe "they click on add item to cart" do
+    before(:each) do
+      @item = create(:item)
+    end
+
+    it "user sees message that item is added to cart" do
+      visit items_path
+
+      click_button "Add to Cart"
+
+      expect(page).to have_content("You now have 1 #{@item.title} in your cart!")
+    end
+
+    it "correctly increments for multiple items" do
+      visit items_path
+
+      click_button "Add to Cart"
+
+      expect(page).to have_content("You now have 1 #{@item.title} in your cart!")
+
+      click_button "Add to Cart"
+
+      expect(page).to have_content("You now have 2 #{@item.title}s in your cart!")
+    end
+  end
+end
