726b4c2b4c336d735cf0aa5b0ed2772c4ce002ea
katyjane8 <katy.jane8@gmail.com>
2018-01-08 12:56:19 -0700

    style order show page - time zone added

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module KnitWits
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = "Mountain Time (US & Canada)"
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
