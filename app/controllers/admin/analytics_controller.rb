58ebe8149d0021517d2b7d288e51b09a030cc17a
katyjane8 <katy.jane8@gmail.com>
2018-01-09 15:33:54 -0700

    ordering by state order count

--- app/controllers/admin/analytics_controller.rb
+++ app/controllers/admin/analytics_controller.rb
@@ -3,9 +3,7 @@ class Admin::AnalyticsController < ApplicationController
     @user_orders = User.user_most_orders if !User.all.empty?
     @categories = Category.all
     @retired_items = Item.retired_items
-    byebug
     if params[:sort] == "count"
-      byebug
       @orders_by_state = Order.state_by_order_count
     else
       @orders_by_state = Order.state_by_alphabetical
