da93bc26fa18e44f3b998ace99c7c5ea58c693ca
maxscores <maxstackhouse@gmail.com>
2018-01-10 12:59:34 -0700

    Add admin item sort tests

--- spec/features/admin/admin_can_sort_and_filter_items_spec.rb
+++ spec/features/admin/admin_can_sort_and_filter_items_spec.rb
@@ -1,29 +1,126 @@
 describe "As an admin when I visit admin items" do
-  it "can sort items by title alphabetically" do
+  before :each do
+    @admin = create(:admin)
+    @item_a = create(:item, title: "A", status: "active", price: 100)
+    @item_m = create(:item, title: "M", status: "active", price: 123)
+    @item_x = create(:item, title: "X", status: "retired", price: 76)
+  end
 
+  it "can sort items by title alphabetically" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+    visit admin_items_path
+    select("Title, A-Z")
+    click_on "Apply"
+
+    within('#0') do
+      expect(page).to have_content("A")
+    end
+    within "#1" do
+      expect(page).to have_content("M")
+    end
+    within "#2" do
+      expect(page).to have_content("X")
+    end
   end
 
   it "can sort items by title reverse alphabetically" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
 
+    visit admin_items_path
+    select("Title, Z-A")
+    click_on "Apply"
+
+    within('#0') do
+      expect(page).to have_content("X")
+    end
+    within "#1" do
+      expect(page).to have_content("M")
+    end
+    within "#2" do
+      expect(page).to have_content("A")
+    end
   end
 
   it "can sort items by price low to high" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
 
+    visit admin_items_path
+    select("Price, Low to High")
+    click_on "Apply"
+
+    within('#0') do
+      expect(page).to have_content("X")
+    end
+    within "#1" do
+      expect(page).to have_content("A")
+    end
+    within "#2" do
+      expect(page).to have_content("M")
+    end
   end
 
   it "can sort items by price high to low" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
 
+    visit admin_items_path
+    select("Price, High to Low")
+    click_on "Apply"
+
+    within('#0') do
+      expect(page).to have_content("M")
+    end
+    within "#1" do
+      expect(page).to have_content("A")
+    end
+    within "#2" do
+      expect(page).to have_content("X")
+    end
   end
 
   it "can filter items by active status" do
-    
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+    visit admin_items_path
+    select("Active")
+    click_on "Apply"
+
+    within('#0') do
+      expect(page).to have_content("A")
+    end
+    within "#1" do
+      expect(page).to have_content("M")
+    end
   end
 
   it "can filter items by retired status" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
 
+    visit admin_items_path
+    select("Retired")
+    click_on "Apply"
+
+    within('#0') do
+      expect(page).to have_content("X")
+    end
   end
 
   it "can sort and filter" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
 
+    visit admin_items_path
+    select("Title, A-Z")
+    select("Active")
+    click_on "Apply"
+
+
+    within('#0') do
+      expect(page).to have_content("A")
+    end
+    within "#1" do
+      expect(page).to have_content("M")
+    end
+
+    expect(page).to_not have_selector('#2')
   end
 end
