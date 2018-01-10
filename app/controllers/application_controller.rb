251a9a4e66ccfedf3b77951951174cee97595201
maxscores <maxstackhouse@gmail.com>
2018-01-10 10:56:54 -0700

    Refactor App Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,15 +28,6 @@ class ApplicationController < ActionController::Base
     current_user && current_user.admin?
   end
 
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def current_admin?
-    current_user && current_user.admin?
-  end
-
   def price_in_dollars(price)
     "$#{sprintf('%.2f', price/100)}"
   end
@@ -45,16 +36,9 @@ class ApplicationController < ActionController::Base
     User.states
   end
 
-
-
-
-
   def logged_in?
     !current_user.nil?
   end
-
-
-
 
     private
 
